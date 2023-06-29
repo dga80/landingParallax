3d7e5c548b5612b508195295eba85131dcaa8731
dga80 <dani.dga@gmail.com>
2023-06-29 11:35:24 +0200

    contact form finished

--- form.js
+++ form.js
@@ -4,3 +4,14 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+function sendMail(){
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  }
+  emailjs.send("service_yxg11nf", "template_fwv11vg", params).then(function (res){
+    alert(`Thank you ${params.name}. Your message has been successfully sent`)
+    // alert("Thank you "+ name + "Your message has been successfully sent!");
+  })
+}
